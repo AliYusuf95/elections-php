f4708c7ccde87d670684735759c0889ab62e7090
Ali Yusuf <aliateeq95@gmail.com>
2024-06-19 02:11:00 +0300

    chore: move db config to .env

--- config.php
+++ config.php
@@ -2,28 +2,15 @@
 
 require 'vendor/autoload.php';
 
-$con = mysqli_init();
-if (!$con) {
-    die("mysqli_init failed");
-}
-
-//mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-mysqli_options($con, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
-mysqli_real_connect($con, "mariadb", "memamali_public", "Sh@[8Pd2z]O$", "memamali_elections", 3306);
-mysqli_set_charset($con, "utf8");
-date_default_timezone_set("Asia/Bahrain");
-
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
 // load variables from environment or use .env file
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $dotenv->required('WS_URL');
 $dotenv->required('INTERNAL_WS_URL');
+$dotenv->required('DB_HOST');
+$dotenv->required('DB_USER');
+$dotenv->required('DB_PASS');
+$dotenv->required('DB_NAME');
 
 // define variables
 define('IS_LOCALHOST', str_contains($_SERVER['HTTP_HOST'], 'localhost'));
@@ -32,3 +19,26 @@ define('INTERNAL_WS_URL', $_ENV['INTERNAL_WS_URL']);
 // check if local environment then use local domain for cookies
 define('INTERNAL_WS_COOKIE_DOMAIN', IS_LOCALHOST ? '' : (empty($_ENV['INTERNAL_WS_COOKIE_DOMAIN']) ? '' : $_ENV['INTERNAL_WS_COOKIE_DOMAIN']));
 define('COOKIE_DOMAIN', IS_LOCALHOST ? '' : (empty($_ENV['COOKIE_DOMAIN']) ? '' : $_ENV['COOKIE_DOMAIN']));
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_PORT', $_ENV['DB_PORT']);
+
+
+$con = mysqli_init();
+if (!$con) {
+    die("mysqli_init failed");
+}
+
+//mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+mysqli_options($con, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
+mysqli_real_connect($con, DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+mysqli_set_charset($con, "utf8");
+date_default_timezone_set("Asia/Bahrain");
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
